c260d44c6a784b51561912d5ad7dc75ddd3ea2e6
onisyan <mknstaiki@gmail.com>
2019-01-15 21:16:45 +0900

    プロトタイプ

---
+++ New Unity Project/Assets/enemyController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class enemyController : MonoBehaviour
+{
+    public int hp;
+    //public SpriteRenderer hpBer;
+    public Text HitPoint;
+
+    private float oneDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  oneDamage = hpBer.transform.localScale.x / hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (input.touchcount > 0)
+        //{
+        //    touch touch = input.gettouch(0);
+
+        //    if (touch.phase == touchphase.began)
+        //    {
+        //        hp = hp - 1;
+        //        debug.log("touch");
+        //    }
+
+        //    rect rect = hpber.rect;
+
+
+        //}
+
+        //if (input.getmousebutton(0))
+        //{
+        //    hp = hp - 1;
+        //    main.instance.attack();
+        //    debug.log("touch");
+        //}
+
+
+
+        // hitpoint.text = hp.tostring();
+
+        /*Transform transform = hpBer.transform;
+
+        Vector3 vec = new Vector3(oneDamage * hp, transform.localScale.y, transform.localScale.z);
+
+        transform.localScale = vec;
+        */
+    }
+}
---
+++ New Unity Project/Assets/main.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main : SingletonMonoBehaviour<main>
+{
+
+    public SpriteRenderer enemy;
+    public Text hitPoint;
+    public int hp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hitPoint.text = hp.ToString();
+    }
+
+    public void attack()
+    {
+        hp--;
+        Debug.Log("attack");
+
+    }
+}
